0bbf47174cce03d26443f4a430b6dbc5a2cc39ac
chikebc <chikecb@outlook.com>
2020-03-23 18:28:01 -0400

    Initial commit

---
+++ src/test/java/com/springboot_302/springboot_302/Springboot302ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_302.springboot_302;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot302ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
